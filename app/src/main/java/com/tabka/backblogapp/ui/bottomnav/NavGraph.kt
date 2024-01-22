66658f17aac46ecae3dceb95e4c8185918bc01cc
Christian Totaro <christian.totaro@stvincent.edu>
2024-01-22 00:52:07 -0500

    Added nested navigation. Added full (almost) navigation for home and search screen

--- app/src/main/java/com/tabka/backblogapp/ui/bottomnav/NavGraph.kt
+++ app/src/main/java/com/tabka/backblogapp/ui/bottomnav/NavGraph.kt
@@ -1,5 +1,7 @@
 package com.tabka.backblogapp.ui.bottomnav
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.Dp
@@ -8,6 +10,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.tabka.backblogapp.ui.screens.HomeScreen
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun BottomNavGraph(navController: NavHostController) {
     NavHost(
