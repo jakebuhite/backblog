d8dd6b9b46a22dd4b5a2c3d1b8572047865c8cd5
jakebuhite <jakebuhite@gmail.com>
2024-02-01 21:05:07 -0500

    Added signup screen testing

--- app/src/androidTest/java/com/tabka/backblogapp/ui/screens/LoginScreenTest.kt
+++ app/src/androidTest/java/com/tabka/backblogapp/ui/screens/LoginScreenTest.kt
@@ -1,6 +1,7 @@
 package com.tabka.backblogapp.ui.screens
 
 import androidx.compose.material3.Surface
+import androidx.compose.ui.test.assertHasClickAction
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertTextEquals
 import androidx.compose.ui.test.junit4.createComposeRule
@@ -13,8 +14,11 @@ import androidx.navigation.compose.ComposeNavigator
 import androidx.navigation.testing.TestNavHostController
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
 import com.tabka.backblogapp.R
 import com.tabka.backblogapp.ui.bottomnav.BottomNavGraph
+import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -41,8 +45,15 @@ class LoginScreenTest {
         }
     }
 
+    @After
+    fun tearDown() {
+        if (Firebase.auth.currentUser != null) {
+            Firebase.auth.signOut()
+        }
+    }
+
     @Test
-    fun loginScreenInView() {
+    fun testLoginScreenInView() {
         val imgId = R.drawable.img_logo_80_80.toString()
         composeTestRule.onNodeWithTag(imgId).assertIsDisplayed()
         composeTestRule.onNodeWithText("BackBlog").assertIsDisplayed()
@@ -59,13 +70,60 @@ class LoginScreenTest {
         composeTestRule.onNodeWithTag("STATUS_MESSAGE").assertDoesNotExist()
 
         // Login Button
-        composeTestRule.onNodeWithTag("LOGIN_BUTTON").assertIsDisplayed()
+        composeTestRule.onNodeWithTag("LOGIN_BUTTON").assertIsDisplayed().assertHasClickAction()
         composeTestRule.onNodeWithText("LOG IN").assertIsDisplayed()
 
         // Go to Signup label/button
         composeTestRule.onNodeWithText("Don't have an account?").assertIsDisplayed()
         composeTestRule.onNodeWithText("Sign up").assertIsDisplayed()
         composeTestRule.onNodeWithTag("GO_TO_SIGNUP_BUTTON").assertIsDisplayed()
+            .assertHasClickAction()
+    }
+
+    @Test
+    fun testLoginButtonClickFailedNoEmail() {
+        // Enter NO email and password
+        composeTestRule.onNodeWithTag("PASSWORD_FIELD").performTextInput("apple123")
+
+        // Click on the login button
+        composeTestRule.onNodeWithTag("LOGIN_BUTTON").performClick()
+
+        composeTestRule.waitUntil(2000) {
+            composeTestRule.onAllNodesWithTag("STATUS_MESSAGE").fetchSemanticsNodes().size == 1
+        }
+
+        composeTestRule.onNodeWithTag("STATUS_MESSAGE").assertTextEquals("Please complete all fields")
+    }
+
+    @Test
+    fun testLoginButtonClickFailedNoPassword() {
+        // Enter NO email and password
+        composeTestRule.onNodeWithTag("EMAIL_FIELD").performTextInput("apple@apple.com")
+
+        // Click on the login button
+        composeTestRule.onNodeWithTag("LOGIN_BUTTON").performClick()
+
+        composeTestRule.waitUntil(2000) {
+            composeTestRule.onAllNodesWithTag("STATUS_MESSAGE").fetchSemanticsNodes().size == 1
+        }
+
+        composeTestRule.onNodeWithTag("STATUS_MESSAGE").assertTextEquals("Please complete all fields")
+    }
+
+    @Test
+    fun testLoginButtonClickFailedAuth() {
+        // Enter NO email and password
+        composeTestRule.onNodeWithTag("EMAIL_FIELD").performTextInput("banana@apple.com")
+        composeTestRule.onNodeWithTag("PASSWORD_FIELD").performTextInput("apple123")
+
+        // Click on the login button
+        composeTestRule.onNodeWithTag("LOGIN_BUTTON").performClick()
+
+        composeTestRule.waitUntil(2000) {
+            composeTestRule.onAllNodesWithTag("STATUS_MESSAGE").fetchSemanticsNodes().size == 1
+        }
+
+        composeTestRule.onNodeWithTag("STATUS_MESSAGE").assertTextEquals("Incorrect email or password.")
     }
 
     @Test
@@ -88,49 +146,4 @@ class LoginScreenTest {
         }
     }
 
-    @Test
-    fun testLoginButtonClickFailedNoEmail() {
-        // Enter NO email and password
-        composeTestRule.onNodeWithTag("PASSWORD_FIELD").performTextInput("apple123")
-
-        // Click on the login button
-        composeTestRule.onNodeWithTag("LOGIN_BUTTON").performClick()
-
-        composeTestRule.waitUntil(2000) {
-            composeTestRule.onAllNodesWithTag("STATUS_MESSAGE").fetchSemanticsNodes().size == 1
-        }
-
-        composeTestRule.onNodeWithTag("STATUS_MESSAGE").assertTextEquals("Please fill all the fields")
-    }
-
-    @Test
-    fun testLoginButtonClickFailedNoPassword() {
-        // Enter NO email and password
-        composeTestRule.onNodeWithTag("EMAIL_FIELD").performTextInput("apple@apple.com")
-
-        // Click on the login button
-        composeTestRule.onNodeWithTag("LOGIN_BUTTON").performClick()
-
-        composeTestRule.waitUntil(2000) {
-            composeTestRule.onAllNodesWithTag("STATUS_MESSAGE").fetchSemanticsNodes().size == 1
-        }
-
-        composeTestRule.onNodeWithTag("STATUS_MESSAGE").assertTextEquals("Please fill all the fields")
-    }
-
-    @Test
-    fun testLoginButtonClickFailedAuth() {
-        // Enter NO email and password
-        composeTestRule.onNodeWithTag("EMAIL_FIELD").performTextInput("banana@apple.com")
-        composeTestRule.onNodeWithTag("PASSWORD_FIELD").performTextInput("apple123")
-
-        // Click on the login button
-        composeTestRule.onNodeWithTag("LOGIN_BUTTON").performClick()
-
-        composeTestRule.waitUntil(2000) {
-            composeTestRule.onAllNodesWithTag("STATUS_MESSAGE").fetchSemanticsNodes().size == 1
-        }
-
-        composeTestRule.onNodeWithTag("STATUS_MESSAGE").assertTextEquals("Incorrect email or password.")
-    }
 }
--- app/src/main/java/com/tabka/backblogapp/util/FirebaseExceptionMapper.kt
+++ app/src/main/java/com/tabka/backblogapp/util/FirebaseExceptionMapper.kt
@@ -6,7 +6,7 @@ fun getErrorMessage(errorCode: String): String {
         "ERROR_INVALID_PASSWORD" -> "Invalid password."
         "ERROR_INVALID_CREDENTIAL" -> "Incorrect email or password."
         "ERROR_CREDENTIAL_ALREADY_IN_USE" -> "An account already exists with the same email address."
-        "ERROR_EMAIL_EXISTS" -> "The email address is already in use by another account."
+        "ERROR_EMAIL_ALREADY_IN_USE" -> "Email already in use."
         "ERROR_USER_DISABLED" -> "The user account has been disabled by an administrator."
         "ERROR_USER_DELETED" -> "User not found."
         else -> "There was an error performing authentication."
