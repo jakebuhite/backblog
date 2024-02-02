958116c3525dadcaefda9c1d726d0e6427c21e5d
jakebuhite <jakebuhite@gmail.com>
2024-02-02 15:49:55 -0500

    Added model serialization tests

--- app/src/test/java/com/tabka/backblogapp/network/models/FriendRequestTest.kt
+++ app/src/test/java/com/tabka/backblogapp/network/models/FriendRequestTest.kt
@@ -5,38 +5,30 @@ import kotlinx.serialization.json.Json
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
-/*class FriendRequestDataTest {
+class FriendRequestTest {
+
+    private val testJson = """{"sender_id":"sender123","target_id":"target456","request_date":"1706751265551","is_complete":true}"""
+    private val friendRequestData = FriendRequestData(
+        senderId = "sender123",
+        targetId = "target456",
+        requestDate = "1706751265551",
+        isComplete = true
+    )
 
     @Test
-    fun testSerialization() {
-        // Given
-        val friendRequestData = FriendRequestData(
-            senderId = "sender123",
-            targetId = "target456",
-            requestDate = "2024-02-02",
-            isComplete = true
-        )
-
-        // When
+    fun testSerializationSuccess() {
         val jsonString = Json.encodeToString(friendRequestData)
 
-        // Then
-        val expectedJson = """{"sender_id":"sender123","target_id":"target456","request_date":"2024-02-02","is_complete":true}"""
-        assertEquals(expectedJson, jsonString)
+        assertEquals(testJson, jsonString)
     }
 
     @Test
-    fun testDeserialization() {
-        // Given
-        val jsonString = """{"sender_id":"sender123","target_id":"target456","request_date":"2024-02-02","is_complete":true}"""
+    fun testDeserializationSuccess() {
+        val testData = Json.decodeFromString<FriendRequestData>(testJson)
 
-        // When
-        val friendRequestData = Json.decodeFromString<FriendRequestData>(jsonString)
-
-        // Then
-        assertEquals("sender123", friendRequestData.senderId)
-        assertEquals("target456", friendRequestData.targetId)
-        assertEquals("2024-02-02", friendRequestData.requestDate)
-        assertEquals(true, friendRequestData.isComplete)
+        assertEquals(testData.senderId, friendRequestData.senderId)
+        assertEquals(testData.targetId, friendRequestData.targetId)
+        assertEquals(testData.requestDate, friendRequestData.requestDate)
+        assertEquals(testData.isComplete, friendRequestData.isComplete)
     }
-}*/
+}
